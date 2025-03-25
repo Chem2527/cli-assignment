2cea2feee04f2c297821d3fbf61756602f5e7a7f
sainexturn2527 <saikrishna.kakumanu@nexturn.com>
2025-03-25 20:27:30 +0530

    ok

--- vulnerable-python-app/app.py
+++ vulnerable-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, I'm Kavita. This is a secure Flask app."
+    return "Hello, I'm Kavita. This app is now Trivy-clean!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7002)  # debug=False by default
